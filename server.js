13fa46d0a3667d9b9f82a20ac4c7b697a6a66b0f
BarryLe <35274721+BarryLe@users.noreply.github.com>
2018-06-13 16:29:42 -0700

    half
    half done

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var allData=require('./allData');
+var fpsData=require('./fpsData');
+var mobData=require('./mobData');
+var racingData=require('./racingData');
+var sportsData=require('./sportsData');
 var app = express();
 var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -12,11 +16,27 @@ app.use(express.static('public'));
 
 
 app.get('/404',function(req,res,next){
-res.status(200).render('404');
+res.status(404).render('404');
 });
 
 
-app.get('/index',function(req,res,next){
+app.get('/sports',function(req,res,next){
+res.status(200).render('home',{game_element:sportsData});
+});
+
+app.get('/racing',function(req,res,next){
+res.status(200).render('home',{game_element:racingData});
+});
+
+app.get('/moba',function(req,res,next){
+res.status(200).render('home',{game_element:mobData});
+});
+
+app.get('/fps',function(req,res,next){
+res.status(200).render('home',{game_element:fpsData});
+});
+
+app.get('/all',function(req,res,next){
 res.status(200).render('home',{game_element:allData});
 });
 
